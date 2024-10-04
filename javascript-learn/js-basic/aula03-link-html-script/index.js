fd6f386e3dd67fe7fcfddfbd3ca375486536543b
JosiasNetto <josiascaldasnetto08@gmail.com>
2024-10-04 12:50:50 -0300

    aulas 03 e 04 js-basic

---
+++ javascript-learn/js-basic/aula03-link-html-script/index.js
@@ -0,0 +1,5 @@
+console.log("OLa mundo")
+console.log("Este treco sera exibido no console do navegador")
+
+//Comando que so funciona no navegador, não no node
+alert("Ola mundo")
---
+++ javascript-learn/js-basic/aula04/index.js
@@ -0,0 +1,10 @@
+//Comumente mais usado
+let name = 'João' //DEclarando e inicializando a variavel
+let year    //Declarando a variavel
+year = 2005 //Inicializando a variavel
+
+//Metdodo mais antigo de declarar
+var name1 = 'Maria'
+
+console.log(name, "nasceu em 1984" )
+console.log("Em 2000", name, "conheceu", name1, "e casou em", year )
