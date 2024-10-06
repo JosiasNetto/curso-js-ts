3232ed031a9c5e4daa886331f78fa8fbc258e6ca
JosiasNetto <josiascaldasnetto08@gmail.com>
2024-10-06 16:11:41 -0300

    Base data types

---
+++ javascript-learn/js-basic/aula07-data-types/index.js
@@ -0,0 +1,26 @@
+//Strings
+const name = 'Luiz';
+const name1 = "Joao";
+const name2 = `Pedro`;
+
+//Number
+const num = 10;
+const num1 = 10.52;
+
+//Undefined
+let nameStudent; //Undefined -> Does not point to any memory space
+
+//Null
+//Explicit way to say that the var does not have a value
+let lastNameStudent = null; //Null -> Does not point to any memory space
+
+//Booleans
+const aproved = true;
+const aproved1 = false;
+
+
+console.log(name, typeof name);
+console.log(num, typeof num);
+console.log(nameStudent, typeof nameStudent);
+console.log(lastNameStudent, typeof lastNameStudent);
+console.log(aproved, typeof aproved);
