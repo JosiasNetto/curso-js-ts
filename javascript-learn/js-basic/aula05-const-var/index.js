1068c313a74be9e9f7f7fe442a207240f91f19af
JosiasNetto <josiascaldasnetto08@gmail.com>
2024-10-07 11:45:10 -0300

    aula 04 e 05 vars

---
+++ javascript-learn/js-basic/aula05-const-var/index.js
@@ -0,0 +1,8 @@
+//A way to create a variable that can`t have its value changed 
+//Need to assign the value of the var, at the time of  declaration, it can't be declared empty
+const name = "Joao"
+
+const firstNumber = 5
+const secondtNumber = 10
+
+const add = firstNumber + secondtNumber
