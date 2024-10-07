1111b3b244184f9935776b1c77c629a57f7e2d22
JosiasNetto <josiascaldasnetto08@gmail.com>
2024-10-06 22:27:49 -0300

    aula 08 aritmetic operators

---
+++ javascript-learn/js-basic/aula08-aritmetic-operators/index.js
@@ -0,0 +1,36 @@
+const num1 = 5;
+const num2 = 10;
+
+//Add
+console.log(num1 + num2)
+
+//Sub
+console.log(num1 - num2)
+
+//Mult
+console.log(num1 * num2)
+
+//Divi
+console.log(num1 / num2)
+
+//Pot
+console.log(num1 ** num2)
+
+//Remain
+console.log(num1 % num2)
+
+let counter = 0;
+//Increment the var value 
+counter++; //by 1
+console.log(counter)
+let pass = 5;
+counter += pass;
+console.log(counter);
+
+//Decrement the var 
+counter = 0
+counter--; //by 1
+console.log(counter);
+counter-= pass;
+console.log(counter);
+
