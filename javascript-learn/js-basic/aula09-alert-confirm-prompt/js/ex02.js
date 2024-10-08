cd57a44eb4923cd507b6517684bbaccabc64838f
JosiasNetto <josiascaldasnetto08@gmail.com>
2024-10-08 16:55:09 -0300

    aula 09- alert, confirm and prompt

---
+++ javascript-learn/js-basic/aula09-alert-confirm-prompt/js/ex02.js
@@ -0,0 +1,5 @@
+let num1 = Number(prompt(`Digite um numero:`))
+
+let num2 = Number(prompt(`Digite um numero:`))
+
+alert(`A soma dos números é : ${num1 + num2}`)
---
+++ javascript-learn/js-basic/aula09-alert-confirm-prompt/js/script.js
@@ -0,0 +1,6 @@
+alert("Message withou return")
+
+confirm("Message that return true or false")
+
+prompt("Return the value typed by the user")
+
