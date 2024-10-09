85066e648e44216fd38cdc7bd5181fd49293597f
JosiasNetto <josiascaldasnetto08@gmail.com>
2024-10-09 14:30:32 -0300

    class 12 Math object

---
+++ javascript-learn/js-basic/aula12-math/index.js
@@ -0,0 +1,15 @@
+//Object Math
+
+let num1 = 9.545678;
+
+console.log(Math.floor(num1));
+
+console.log(Math.ceil(num1));
+
+console.log(Math.round(num1));
+
+console.log(Math.max(1, 2 , 3, 12, 123, 200));
+
+console.log(Math.min(1, 2 , 3, 12, 123, 200));
+
+console.log(Math.random());
