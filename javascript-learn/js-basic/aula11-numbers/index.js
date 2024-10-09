01666647f19f5658cd5e56e61d0a552c86f04af4
JosiasNetto <josiascaldasnetto08@gmail.com>
2024-10-09 14:19:30 -0300

    class 11 numbers

---
+++ javascript-learn/js-basic/aula11-numbers/index.js
@@ -0,0 +1,12 @@
+let num1 = 1;   //number
+let num2 = 2.52141234123134;    //Number
+
+console.log(num1.toString() + num2.toString())  //Show as string
+
+console.log(num1.toString(2)) //Show the binary form
+
+console.log(num2.toFixed(2))  //Limit the number of decimal numbers
+
+console.log(Number.isInteger(num1)) //Return if the number is a integer 
+
+
