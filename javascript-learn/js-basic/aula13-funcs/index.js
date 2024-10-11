22188599c810c3e33afdd4d39a754b030a0f8ab2
JosiasNetto <josiascaldasnetto08@gmail.com>
2024-10-11 16:25:33 -0300

    class 13 functions

---
+++ javascript-learn/js-basic/aula13-funcs/index.js
@@ -0,0 +1,18 @@
+function saudacao(name1) {
+    console.log(`Bom dia ${name1}!`)
+}
+
+function soma(x, y){
+    const resultado = x + y;
+    return resultado;
+}
+
+//Arrow function
+const raiz = (n) => {
+    return n ** 0.5;
+ };
+
+const name1 =  `Beta`
+saudacao(name1)
+
+console.log(soma(3,1  ))
