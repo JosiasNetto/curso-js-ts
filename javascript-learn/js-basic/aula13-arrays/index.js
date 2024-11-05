9c4c4df79f162f1d08eeace5025522cd46e599fe
JosiasNetto <josiascaldasnetto08@gmail.com>
2024-11-05 15:39:26 -0300

    Arrays

---
+++ javascript-learn/js-basic/aula13-arrays/index.js
@@ -0,0 +1,14 @@
+const alunos = [`Joca`, `Beta`, `Pepo`] //Array
+console.log(alunos)
+
+alunos.push(`Tony`) //Add a element in the final of the array
+console.log(alunos)
+
+alunos.unshift(`Mars`)  //Add a element at the start of the array
+console.log(alunos)
+
+const removed = alunos.pop() //Delete the final element of the array
+console.log(alunos)
+
+const removed2 = alunos.shift() //Delete the initial element of the array
+console.log(alunos)
