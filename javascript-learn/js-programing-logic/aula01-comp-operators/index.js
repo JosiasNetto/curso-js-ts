5708555845c6548cb3180a4071eb37b5131318a7
JosiasNetto <josiascaldasnetto08@gmail.com>
2024-11-05 16:45:30 -0300

    Comparasion operators

---
+++ javascript-learn/js-programing-logic/aula01-comp-operators/index.js
@@ -0,0 +1,11 @@
+/*
+Comparative operators
+    > Bigger then
+    <= Bigger then or equal to
+    < Less then
+    <= Less then or equal to
+    == Equaity (value)
+    === Dtrict equality (value and type)
+    != Diferent (value)
+    !== Diferent (value and type)
+*/
